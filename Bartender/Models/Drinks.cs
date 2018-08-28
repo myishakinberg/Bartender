17967f76def2206a579361815f3fbfa5edf427f7
myishakinberg <mskinberg@gmail.com>
2018-08-27 23:28:10 -0400

    Edited index and added drinks model

---
+++ Bartender/Models/Drinks.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bartender.Models
+{
+    public class Drinks
+    {
+        public string Name { get; set; }
+        public int AlcContent { get; set; }
+        public int Cost { get; set; }
+        public string Description { get; set; }
+        public List<Drinks> Menu = new List<Drinks>();
+
+        public Drinks()
+        {
+            Drinks Mojito = new Drinks();
+            Mojito.Name = "Mojito";
+            Mojito.AlcContent = 15;
+            Mojito.Cost = 9;
+            Mojito.Description = "Mojito is a traditional Cuban highball. " +
+                "Traditionally, a mojito is a cocktail that consists of five " +
+                "ingredients: white rum, sugar, lime juice, soda water, and mint.";
+
+            Drinks Sangria = new Drinks();
+            Sangria.Name = "Sangria";
+            Sangria.AlcContent = 14;
+            Sangria.Cost = 12;
+            Sangria.Description = "Sangria is an alcoholic beverage of " +
+                "Spanish origin. A punch, the sangria traditionally" +
+                " consists of red wine and chopped fruit, " +
+                "often with other ingredients such as orange juice or brandy.";
+
+            Drinks Bourbon = new Drinks();
+            Bourbon.Name = "Bourbon Whiskey";
+            Bourbon.AlcContent = 40;
+            Bourbon.Cost = 17;
+            Bourbon.Description = "Bourbon whiskey is a type of American " +
+            "whiskey, a barrel-aged distilled spirit made " +
+            "primarily from corn. ";
+
+            Drinks Champange = new Drinks();
+            Champange.Name = "Champange Cocktail";
+            Champange.AlcContent = 12;
+            Champange.Cost = 11;
+            Champange.Description = "A champagne cocktail is an alcoholic " +
+             "drink made with sugar, Angostura bitters, Champagne," +
+            " brandy and a maraschino cherry as a garnish. ";
+
+            Drinks French = new Drinks();
+            French.Name = "French 75";
+            French.AlcContent = 15;
+            French.Cost = 14;
+            French.Description = "French 75 is a cocktail made from gin, " +
+            "Champagne, lemon juice, and sugar. It is also called a 75" +
+            " Cocktail, or in French simply a Soixante Quinze.";
+        }
+
+    }
+}
